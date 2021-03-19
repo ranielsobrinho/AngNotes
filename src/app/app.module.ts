244239771bacc341e4f14327b534224669fe0baf
Raniel <raniel25sobrinho@gmail.com>
2021-03-19 16:37:47 -0300

    Add fontawesome for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
