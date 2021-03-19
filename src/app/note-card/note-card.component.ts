1398aaa3bc35945b3c812dedcfe793e6acc07794
Raniel <raniel25sobrinho@gmail.com>
2021-03-19 20:03:11 -0300

    Add input decorator to interacts with DOM

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-card.component.css']
 })
 export class NoteCardComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() body: string;
 
   constructor() { }
 
