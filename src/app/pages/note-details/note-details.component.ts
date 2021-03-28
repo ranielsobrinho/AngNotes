5152c10ede329ba6050de192db5d3d61a011c800
Raniel <raniel25sobrinho@gmail.com>
2021-03-28 14:15:29 -0300

    Add form builder

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Note } from 'src/models/notes.model';
 
 @Component({
   selector: 'app-note-details',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteDetailsComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  public notes: Note[] = [];
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])],
+      body: ['', Validators.compose([
+        Validators.minLength(10),
+        Validators.maxLength(90),
+        Validators.required
+      ])]
+    });
+  }
 
   ngOnInit(): void {
   }
