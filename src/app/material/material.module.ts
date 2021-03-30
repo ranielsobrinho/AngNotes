845821da5a46fcc6c3b767448e145427a35da93c
Raniel <raniel25sobrinho@gmail.com>
2021-03-29 23:10:29 -0300

    Add dialog module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
 
 const MaterialComponent = [
   MatButtonModule,
   MatIconModule,
   MatToolbarModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatDialogModule
 ];
 
 @NgModule({
