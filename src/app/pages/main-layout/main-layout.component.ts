88d283155ada29cc3e392be062864f93f18c9760
Raniel <raniel25sobrinho@gmail.com>
2021-03-29 23:11:05 -0300

    Add open dialog method in ts component

--- src/app/pages/main-layout/main-layout.component.ts
+++ src/app/pages/main-layout/main-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { FormDialogComponent } from '../form-dialog/form-dialog.component';
 
 @Component({
   selector: 'app-main-layout',
@@ -8,9 +10,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+
+  addNote(): void {
+    const dialogRef = this.dialog.open(FormDialogComponent, {
+      width: '250px',
+    });
+
+    // tslint:disable-next-line: deprecation
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
 
 }
