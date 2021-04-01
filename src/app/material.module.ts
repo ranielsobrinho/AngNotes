4e4d628a927fff171da3e022d5aed5b038b19b7c
Raniel <raniel25sobrinho@gmail.com>
2021-03-31 23:09:27 -0300

    Editted file

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDialogModule} from '@angular/material/dialog';
 
-const MaterialComponent = [
-  MatButtonModule,
-  MatIconModule,
-  MatToolbarModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDialogModule
-];
 
 @NgModule({
-  imports: [MaterialComponent],
-  exports: [MaterialComponent]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule
+  ]
 })
 export class MaterialModule { }
