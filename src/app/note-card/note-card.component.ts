40aa4d06635a73cc399a4c9d7223e1ad9953d3d8
Raniel <raniel25sobrinho@gmail.com>
2021-04-03 17:38:10 -0300

    Add event emitter to delete in note-list component

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -10,9 +10,16 @@ export class NoteCardComponent implements OnInit {
   @Input() title: string;
   @Input() body: string;
 
+  // tslint:disable-next-line: no-output-rename
+  @Output('delete') deleteEvent: EventEmitter<void> = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  exclude(): void {
+    this.deleteEvent.emit();
   }
 
 }
