9a1fef5641f15b73b7812d46ccc5a6493ddd9246
Raniel <raniel25sobrinho@gmail.com>
2021-04-03 11:14:01 -0300

    Declaring the routing components by importing the component in the routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotesListComponent } from './pages/notes-list/notes-list.component';
-import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NoteCardComponent } from './note-card/note-card.component';
-import { NoteDetailsComponent } from './pages/note-details/note-details.component';
 import { FormDialogComponent } from './pages/form-dialog/form-dialog.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotesListComponent,
-    MainLayoutComponent,
+    routingComponents,
     NoteCardComponent,
-    NoteDetailsComponent,
     FormDialogComponent
   ],
   imports: [
