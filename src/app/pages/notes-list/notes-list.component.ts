d6d0c7dd8592bbf4ddf02bd14cd51f279884ae7c
Raniel <raniel25sobrinho@gmail.com>
2021-04-03 17:40:18 -0300

    Add animation and delete method

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,11 +1,59 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { setClassMetadata } from '@angular/core/src/r3_symbols';
 import { NotesService } from 'src/app/shared/notes.service';
 import { Note } from 'src/models/notes.model';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.css']
+  styleUrls: ['./notes-list.component.css'],
+  animations: [
+    trigger('itemAnimate', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0
+        }),
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingLeft: '*',
+          paddingRight: '*'
+        })),
+        animate(68)
+      ]),
+      transition('* => void', [
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0
+        })),
+        animate('150ms ease-out', style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          'margin-bottom': '0',
+        }))
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
@@ -16,6 +64,10 @@ export class NotesListComponent implements OnInit {
   ngOnInit(): any {
     // retrieve all notes from NotesService
     this.notes = this.notesService.getAll();
+  }
+
+  deleteNote(id: number): void {
+    this.notesService.delete(id);
   }
 
 }
