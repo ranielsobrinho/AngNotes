ebb88cdb681c5e6f5c68d7a395d3d380affa5134
Raniel <raniel25sobrinho@gmail.com>
2021-04-04 20:57:26 -0300

    Add new route to update the notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { UpdateNoteComponent } from './pages/update-note/update-note.component';
 
 const routes: Routes = [
   {path: '', component: MainLayoutComponent, children: [
-    {path: '', component: NotesListComponent}
+    {path: '', component: NotesListComponent},
+    {path: ':id', component: UpdateNoteComponent}
   ]}
 ];
 
@@ -14,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [MainLayoutComponent, NotesListComponent];
+export const routingComponents = [MainLayoutComponent, NotesListComponent, UpdateNoteComponent];
