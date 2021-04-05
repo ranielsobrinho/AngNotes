733eac27956d84408346d1df3a187398109ae69c
Raniel <raniel25sobrinho@gmail.com>
2021-04-04 22:47:12 -0300

    Add update method and start to auto render the content of the note

--- src/app/pages/update-note/update-note.component.ts
+++ src/app/pages/update-note/update-note.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NotesService } from 'src/app/shared/notes.service';
+import { Note } from 'src/models/notes.model';
 
 @Component({
   selector: 'app-update-note',
@@ -11,10 +12,13 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class UpdateNoteComponent implements OnInit {
 
   public form: FormGroup;
+  public note: Note;
+  public noteId: number;
 
   constructor(private fb: FormBuilder,
               private notesService: NotesService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
     this.form = this.fb.group({
       title: ['', Validators.compose([
         Validators.minLength(3),
@@ -30,9 +34,20 @@ export class UpdateNoteComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    // tslint:disable-next-line: deprecation
+    this.route.params.subscribe((params: Params) => {
+      this.note = Note[params.id];
+      if (params.id) {
+        this.note = this.notesService.get(params.id);
+        this.noteId = params.id;
+      }
+    });
   }
 
   updateNote(): void {
-    console.log('Atualizado');
+    const title = this.form.controls.title.value;
+    const body = this.form.controls.body.value;
+    this.notesService.update(this.noteId, title, body);
+    this.router.navigateByUrl('/');
   }
 }
