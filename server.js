65803081003298c2eeb2cae0b96c0a099df653c6
Raniel <raniel25sobrinho@gmail.com>
2021-05-08 23:38:11 -0300

    Deploy on Heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/AngNotes'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/AngNotes/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado na porta " + PORT);
+});
